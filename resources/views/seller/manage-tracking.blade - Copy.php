0435f48e96b221b5b23629d0517448eaf96a6661
gaji asif <asif01665@yahoo.com>
2021-04-04 04:13:41 +0600

    all

--- resources/views/seller/manage-tracking.blade - Copy.php
+++ resources/views/seller/manage-tracking.blade - Copy.php
@@ -1,32 +1,7 @@
 
- <!DOCTYPE html>
-            <html lang="en">
+<x-app-layout>
 
-            <head>
-              <meta charset="utf-8">
-              <meta name="viewport" content="width=device-width, initial-scale=1">
-              <meta name="csrf-token" content="{{ csrf_token() }}">
-
-              <title>DoDotracking - Manage Tracking</title>
-
-              <!-- Fonts -->
-              <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-
-              <!-- Styles -->
-           <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-              <!-- Chart -->
-
-            </head>
-
-            <body class="font-sans antialiased">
-              <div class="min-h-screen bg-gray-100">
-
-                @include('layouts.navigation')
-               
-
-<main class="grid gap-x-8 gap-y-8 grid-cols-12 py-8 md:py-12 px-8 justify-center md:px-12 ">
+  @section('title', 'Manage Tracking')
   <link href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css" rel="stylesheet">
   <link href="https://cdn.datatables.net/1.10.21/css/dataTables.bootstrap4.min.css" rel="stylesheet">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
@@ -38,133 +13,97 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 
-  <!--   only bootstrap by asif -->
+<!--   only bootstrap -->
 
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
 
-<style>
 
-   a:hover{
-    text-decoration: none;
-  }
+  <style>
+     a:hover{
+      text-decoration: none;
+    }
 
-  [x-cloak] {
-    display: none;
-  }
+    [x-cloak] {
+      display: none;
+    }
 
-  .duration-300 {
-    transition-duration: 300ms;
-  }
+    .duration-300 {
+      transition-duration: 300ms;
+    }
 
-  .ease-in {
-    transition-timing-function: cubic-bezier(0.4, 0, 1, 1);
-  }
+    .ease-in {
+      transition-timing-function: cubic-bezier(0.4, 0, 1, 1);
+    }
 
-  .ease-out {
-    transition-timing-function: cubic-bezier(0, 0, 0.2, 1);
-  }
+    .ease-out {
+      transition-timing-function: cubic-bezier(0, 0, 0.2, 1);
+    }
 
-  .scale-90 {
-    transform: scale(.9);
-  }
+    .scale-90 {
+      transform: scale(.9);
+    }
 
-  .scale-100 {
-    transform: scale(1);
-  }
+    .scale-100 {
+      transform: scale(1);
+    }
 
-  .modal-hide {
-    display: none !important;
-  }
-  .tracking_table{
-    text-align: center;
-  }
+    .modal-hide {
+      display: none !important;
+    }
+    .tracking_table{
+      text-align: center;
+    }
 
-  .btns {
-    background-color: DodgerBlue;
-    border: none;
-    color: white;
-    padding: 12px 16px;
-    font-size: 16px;
-    cursor: pointer;
-    border-radius: 5px;
-  }
+    .btns {
+  background-color: DodgerBlue;
+  border: none;
+  color: white;
+  padding: 12px 16px;
+  font-size: 16px;
+  cursor: pointer;
+  border-radius: 5px;
+}
 
-  .btns_limit{
+.btns_limit{
     background-color: #10B880;
-    border: none;
-    color: white;
-    padding: 12px 16px;
-    font-size: 16px;
-    cursor: pointer;
-    border-radius: 5px;
-  }
+  border: none;
+  color: white;
+  padding: 12px 16px;
+  font-size: 16px;
+  cursor: pointer;
+  border-radius: 5px;
+}
 
-  /* Darker background on mouse-over */
-  .btns:hover {
-    background-color: RoyalBlue;
-  }
-</style>
+/* Darker background on mouse-over */
+.btns:hover {
+  background-color: RoyalBlue;
+}
+ </style>
+
 <?php 
 $max_limits  = '';
 $message = '';
-$percantage_value = '';
 if(!empty(Auth::user()->package_id)){
       $max_limits = $this_users_limit->max_limit;
-      //$message =  '(Used - '.count($total_tracking_orders).' , Max Limit-' .$max_limits. ')';
-      if($max_limits>0){
-        $percantage_value = (count($total_tracking_orders)*100)/$max_limits;
-      
-      }
+      $message =  '(Used - '.count($total_tracking_orders).' , Max Limit-' .$max_limits. ')';
     }          
 ?>
 
 
+<x-card title="Manage Tracking {{ $message }}">
 
-<div
-class="bg-white rounded-md w-full overflow-hidden col-span-12 md:col-span-12 shadow">
-<div class="pb-2">
-  <div class="p-6 rounded-lg bg-white">
-    <div class="md:justify-between">
-   @if(!empty($users_packages))
-        <h2 class="text-xl text-gray-800 font-bold leading-tight">Manage Tracking {{ $message }}<a style="color: #28a745;" class="pull-right">{{count($total_tracking_orders)}}/{{$max_limits}}</a></h2>
-
-@endif
-    
-    </div>
-
-@if(empty($users_packages))
-
- <div class="md:justify-between" style="margin-top: 15px;">
-   
-        <h2 class="text-xl text-gray-800 font-bold leading-tight">Manage Tracking <a style="color: #28a745;" class="pull-right"></a></h2>
-
-    
-    </div>
-  <div class="alert alert-danger col-lg-12" role="alert">
- You have No packages purchased yet. please choose a package from here.
+  @if(empty($users_packages))
+  <x-alert-danger>You have No packages purchased yet. please choose a package from here.
     <a href="{{route('your_packages')}}" style="text-decoration: underline; font-weight: bold;">All packages</a>
-</div>
+  </x-alert-danger>
   @else
 
-<div class="progress" style="height: 20px">
-  <div
-    class="progress-bar bg-success"
-    role="progressbar"
-    style="width: {{(int)$percantage_value}}%"
-    aria-valuenow="{{(int)$percantage_value}}"
-    aria-valuemin="0"
-    aria-valuemax="100"
-  >
-    {{(int)$percantage_value}}%
-  </div>
-</div>
-    <div class="my-8 relative">
-      <div class="mt-4">
 
-         @if(session('danger'))
+   <div class="mt-4">
+     @if(session('danger'))
         <x-alert-danger>{{ session('danger') }}</x-alert-danger>
       @endif
       @if(session('success'))
@@ -180,43 +119,57 @@ class="bg-white rounded-md w-full overflow-hidden col-span-12 md:col-span-12 sha
         </x-alert-danger>
       @endif
 
-        <button style="margin-top: -15px;" class="btns" id="BtnInsert"><i class="fa fa-plus"></i> Add</button>
+      <button style="margin-top: -15px;" class="btns" id="BtnInsert"><i class="fa fa-plus"></i> Add</button>
 
-        <button class="btns" id="BtnImport"><i class="fa fa-upload"></i> Import</button>
+      <button class="btns" id="BtnImport"><i class="fa fa-upload"></i> Import</button>
+
+      
+
+      <button style="float: right;" class="btns_limit" style="background-color: #00c851 !important;">Limit - {{$max_limits}} (Per Day)</button>
 
 
-
-       <!--  <button style="float: right;" class="btns_limit" style="background-color: #00c851 !important;">Limit - {{$max_limits}} (Per Day)</button> -->
-
-
-    
-  <div class="my-4">
+      <!-- <x-button color="green" id="BtnInsert">
+        <p class="mr-1">Add</p>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" width="18px" height="18px">
+          <path d="M0 0h24v24H0z" fill="none" />
+          <path
+          d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm5 11h-4v4h-2v-4H7v-2h4V7h2v4h4v2z" />
+        </svg>
+      </x-button> -->
+      <!--    <x-button color="green" id="BtnImport">
+            <p class="mr-1">Import</p>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" width="18px" height="18px">
+              <path d="M0 0h24v24H0z" fill="none" />
+              <path d="M9 16h6v-6h4l-7-7-7 7h4zm-4 2h14v2H5z" />
+            </svg>
+          </x-button> -->
+      <div class="my-4">
         <x-input type="date" name="input-date" id="inputDate">
         </x-input>
       </div>
 
-<!-- insert modal -->
-<div class="modal-insert modal-hide">
-  <div style="background-color: rgba(0,0,0,0.5)"
-  class="overflow-auto fixed inset-0 z-10 flex items-center justify-center">
-  <div class="bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg pt-4 pb-6 text-left px-6"
-  x-transition:enter="ease-out duration-300" x-transition:enter-start="opacity-0 scale-90"
-  x-transition:enter-end="opacity-100 scale-100">
+      <!-- insert modal -->
+      <div class="modal-insert modal-hide">
+        <div style="background-color: rgba(0,0,0,0.5)"
+        class="overflow-auto fixed inset-0 z-10 flex items-center justify-center">
+        <div class="bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg pt-4 pb-6 text-left px-6"
+        x-transition:enter="ease-out duration-300" x-transition:enter-start="opacity-0 scale-90"
+        x-transition:enter-end="opacity-100 scale-100">
 
-  <div class="flex justify-between items-center pb-3">
-    <p class="text-2xl font-bold">Add Tracking</p>
+        <div class="flex justify-between items-center pb-3">
+          <p class="text-2xl font-bold">Add Tracking</p>
+          {{-- tombol close --}}
+          <div class="cursor-pointer z-50" id="closeModalInsert">
+            <svg class="fill-current text-black" xmlns="http://www.w3.org/2000/svg" width="18"
+            height="18" viewBox="0 0 18 18">
+            <path
+            d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z">
+          </path>
+        </svg>
+      </div>
+    </div>
 
-    <div class="cursor-pointer z-50" id="closeModalInsert">
-      <svg class="fill-current text-black" xmlns="http://www.w3.org/2000/svg" width="18"
-      height="18" viewBox="0 0 18 18">
-      <path
-      d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z">
-    </path>
-  </svg>
-</div>
-</div>
-
- <form method="POST" action="{{ route('insert tracking') }}" id="form-insert">
+    <form method="POST" action="{{ route('insert tracking') }}" id="form-insert">
       @csrf
       <div>
         <x-label>
@@ -245,9 +198,8 @@ class="bg-white rounded-md w-full overflow-hidden col-span-12 md:col-span-12 sha
       <x-label>
         Shipper
       </x-label>
-      <!-- <x-select name="shipper" id="shipper" required> -->
-        <select name="shipper" id="shipper" required="required" style="width: 100%;">
-        <option value="">Select Shipper</option>
+      <x-select name="shipper" id="shipper">
+        <option disabled selected value="0">Select Shipper</option>
         @foreach ($shippers as $shipper)
         <option value="{{ $shipper->id }}"> {{ $shipper->name }} </option>
         @endforeach
@@ -273,7 +225,7 @@ class="bg-white rounded-md w-full overflow-hidden col-span-12 md:col-span-12 sha
 
   <div class="flex justify-between items-center pb-3">
     <p class="text-2xl font-bold">Import Tracking</p>
-    
+    {{-- tombol close --}}
     <div class="cursor-pointer z-50" id="closeModalImport">
       <svg class="fill-current text-black" xmlns="http://www.w3.org/2000/svg" width="18"
       height="18" viewBox="0 0 18 18">
@@ -297,12 +249,12 @@ class="bg-white rounded-md w-full overflow-hidden col-span-12 md:col-span-12 sha
     <x-label>
       Shipper
     </x-label>
-    <select name="shipper" id="shipper" required="required" style="width: 100%;">
-      <option value="">Select Shipper</option>
+    <x-select name="shipper" id="shipper">
+      <option disabled selected value="0">Select Shipper</option>
       @foreach ($shippers as $shipper)
       <option value="{{ $shipper->id }}"> {{ $shipper->name }} </option>
       @endforeach
-    </select>
+    </x-select>
   </div>
   <div class="flex justify-end mt-4">
     <x-button color="blue" type="submit" id="BtnImportSubmit">Save</x-button>
@@ -315,7 +267,7 @@ class="bg-white rounded-md w-full overflow-hidden col-span-12 md:col-span-12 sha
 
 
 
-
+{{-- update modal --}}
 <div class="modal-update modal-hide">
   <div style="background-color: rgba(0,0,0,0.5)"
   class="overflow-auto fixed inset-0 z-10 flex items-center justify-center">
@@ -325,7 +277,7 @@ class="bg-white rounded-md w-full overflow-hidden col-span-12 md:col-span-12 sha
 
   <div class="flex justify-between items-center pb-3">
     <p class="text-2xl font-bold">Update Tracking</p>
-    
+    {{-- tombol close --}}
     <div class="cursor-pointer z-50" id="closeModalUpdate">
       <svg class="fill-current text-black" xmlns="http://www.w3.org/2000/svg" width="18"
       height="18" viewBox="0 0 18 18">
@@ -348,7 +300,7 @@ class="bg-white rounded-md w-full overflow-hidden col-span-12 md:col-span-12 sha
 <form action="#">
   <div class="flex justify-between flex-col">
     <div class="overflow-x-auto">
-      <table class="table-auto border-collapse w-full border mt-4" id="datatable" style="text-align: center;">
+       <table class="table-auto border-collapse w-full border mt-4" id="datatable" style="text-align: center;">
         <thead class="border bg-green-300" style="text-align: center;">
           <tr class="rounded-lg text-sm font-medium text-gray-700 text-left">
             <th class="px-4 py-2 border-2 tracking_table">Date/Time</th>
@@ -366,12 +318,8 @@ class="bg-white rounded-md w-full overflow-hidden col-span-12 md:col-span-12 sha
   </div>
 
 </form>
-</div>
-</div>
-</div>
-</div>
-@endif
-
+  @endif
+</x-card>
 
 
 <script type="text/javascript" src="/js/excel.js"></script>
@@ -505,9 +453,5 @@ class="bg-white rounded-md w-full overflow-hidden col-span-12 md:col-span-12 sha
                      });
 
                    </script>
-            </main>
-          </div>
-        </body>
 
-        </html>
-
+                 </x-app-layout>
